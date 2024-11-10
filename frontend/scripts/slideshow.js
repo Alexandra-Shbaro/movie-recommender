075d110c323b8cafdaa8a49c2518e3b1d7df7bde
Alexandra Shbaro <alexandrashbaro@gmail.com>
2024-11-10 03:24:55 +0200

    added navbar and hero section slideshow

---
+++ frontend/scripts/slideshow.js
@@ -0,0 +1,20 @@
+let slideIndex = 0;
+const slides = [
+    'assets/images/Thething.jpeg',
+    'assets/images/noel.jpg',
+    'assets/images/venom.jpeg',
+    'assets/images/theCrow.jpg'
+];
+
+const container = document.querySelector('.hero');
+
+function showSlides() {
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1; }
+    container.style.backgroundImage = `url(${slides[slideIndex - 1]})`;
+
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+// Initialize the slideshow
+showSlides();
